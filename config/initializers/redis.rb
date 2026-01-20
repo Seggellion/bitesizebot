350340300e26fe2a6681df07f5ef85a62996cd13
Seggellion <dustin@altama.ca>
2026-01-19 18:28:36 -0800

    redis change

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,8 @@
 # config/initializers/redis.rb
 if Rails.env.production?
-  Redis.current = Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+  # Create a global constant instead of using the deprecated Redis.current
+  REDIS = Redis.new(
+    url: ENV['REDIS_URL'], 
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  )
 end
